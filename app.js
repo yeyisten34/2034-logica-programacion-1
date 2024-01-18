042cf606737ea6a93978b8abbd4e5f189a4453dc
Nehemias Reyes Citalan <nehemis16@gmail.com>
2024-01-18 11:35:32 -0600

    inicio

---
+++ app.js
@@ -0,0 +1,10 @@
+let numeroSecreto = 6;
+let numeroUsuario = prompt("Me indicas un número por favor");
+
+console.log(numeroUsuario)
+
+if (numeroUsuario == numeroSecreto){
+    alert('excelente el ' + numeroSecreto + 'es igual a ' + numeroSecreto);
+}else{
+    alert('fallaste');
+}
