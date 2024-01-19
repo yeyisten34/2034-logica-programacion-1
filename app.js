8ffec806c1ae491075d9cae3a928f741101564ac
Nehemias Reyes Citalan <nehemis16@gmail.com>
2024-01-19 11:20:35 -0600

    avance app.js

--- app.js
+++ app.js
@@ -1,15 +1,35 @@
-let numeroSecreto = 6;
-let numeroUsuario = prompt("Me indicas un número del 1 al 10 por favor");
+//Variables
+let numeroSecreto = Math.floor(Math.random()*10)+1;
+let numeroUsuario = 0;
+let intentos = 1;
+//let palabraVeces = 'vez';
+let maximosIntentos = 3;
 
-console.log(numeroUsuario)
 
-if (numeroUsuario == numeroSecreto){
-alert(`Acertaste, el numero es: ${numeroUsuario}`);
-}else{
-    if(numeroUsuario > numeroSecreto){
-        alert('El numero secreto es menor');
-    }else{
-        alert(' El numero secreto es mayor')
+while (numeroUsuario != numeroSecreto) {
+    numeroUsuario = parseInt(prompt("Me indicas un número entre 1 y 10 por favor:"));
+
+    console.log(typeof(numeroUsuario));
+    if (numeroUsuario == numeroSecreto) {
+        //Acertamos, fue verdadera la condición
+        alert(`Acertaste, el número es: ${numeroUsuario}. Lo hiciste en ${intentos} ${intentos == 1 ? 'vez' : 'veces' }`);
+    } else {
+        if (numeroUsuario > numeroSecreto) {
+            alert('El número secreto es menor');
+        } else {
+            alert('El número secreto es mayor');
+        }
+        //Incrementamos el contador cuando no acierta
+        //intentos = intentos + 1;
+        //intentos += 1;
+        intentos++;
+
+        //palabraVeces = 'veces';
+        if (intentos > maximosIntentos) {
+            alert(`Llegaste al número máximo de ${maximosIntentos} intentos`);
+            break;
+        }
+        //La condición no se cumplió
+        //alert('Lo siento, no acertaste el número');
     }
 }
-
