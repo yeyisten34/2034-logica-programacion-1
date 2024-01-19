a76443b686e69f1db15096571d030b341f2fead1
Nehemias Reyes Citalan <nehemis16@gmail.com>
2024-01-19 11:19:48 -0600

    desafio contadores

---
+++ retos-y-desafios/3-desafio-contadores.js
@@ -0,0 +1,51 @@
+
+
+/*
+1. Crea un contador que comience en 1 y 
+   vaya hasta 10 usando un bucle 'while'. Muestra cada número.
+*/
+
+/*
+let contador = 1;
+
+while (contador < 11){
+
+    console.log(contador)
+    contador = contador+1;
+}*/
+
+
+/*
+2. Crea un contador que comience en 10 
+y vaya hasta 0 usando un bucle 'while'. 
+Muestra cada número.
+*/
+
+/*
+let contador = 10;
+
+while (contador >= 0){
+
+    console.log(contador)
+    contador = contador-1;
+}
+*/
+
+/*
+3. Crea un programa de cuenta regresiva. 
+    Pide un número y cuenta desde 0 hasta 
+    ese número utilizando un bucle 'while' en la consola del navegador.
+*/
+
+/*let numeroIngresado = prompt('Ingresa un numero mayor a cero;');
+
+
+let contador = 0;
+
+while (contador <= numeroIngresado){
+
+    console.log(contador)
+    contador = contador+1;
+}*/
+
+
